67936d2e769a0bd82d42b032ffb4bdda957231f2
Mork <mawwwrk@gmail.com>
2022-03-04 02:21:11 +0000

    extracted header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import logo from "../logo.svg";
+import { ScrollingBanner } from "./ScrollingBanner";
 
 export default function Header() {
   return (
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
+      <ScrollingBanner />
     </header>
   );
 }
